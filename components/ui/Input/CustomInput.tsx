bea3b6d4048ad830808c6b8a396fdd8e0ac5df32
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-12-09 22:08:21 +0200

    refactor: Add CustomInput component for enhanced input handling with optional password visibility

---
+++ components/ui/Input/CustomInput.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { VscEye } from "react-icons/vsc";
+
+type props = {
+	type: string;
+	placeholder: string;
+	name: string;
+	id: string;
+	value: string;
+};
+
+export default function CustomInput({ type, placeholder, name, id, value }: props) {
+	return (
+		<>
+			<div className='relative'>
+				<input
+					className='bg-[#F9F9F9] rounded-lg w-full p-3 text-primary-text-light text-sm shadow-[0px_10px_20px_0px_rgba(68,97,242,0.05)] border focus-visible:border-primary-default focus-visible:border-2 outline-none'
+					placeholder={placeholder}
+					type={type}
+					name={name}
+					id={id}
+					defaultValue={value}
+				/>
+				{type === "password" && <VscEye className='absolute top-1/2 right-3 transform -translate-y-1/2 text-primary-text-dark text-xl cursor-pointer' />}
+			</div>
+		</>
+	);
+}
