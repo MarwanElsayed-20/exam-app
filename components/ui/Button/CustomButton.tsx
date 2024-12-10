ed8e8bae3d56b59ce42406a7e943b12eea9064e3
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-12-10 22:22:35 +0200

    feat: Add loading state to CustomButton component with visual feedback

--- components/ui/Button/CustomButton.tsx
+++ components/ui/Button/CustomButton.tsx
@@ -8,12 +8,16 @@ type Props = {
 	kind?: "primary" | "secondary";
 	href?: string;
 	alt?: string;
+	loading?: boolean;
 };
 
-export default function CustomButton({ text, type = "submit", kind = "primary", href, alt = "button" }: Props) {
-	const classes = `shadow-[0px_18px_30px_0px_rgba(47,28,28,0.1)] p-3 gap-2 flex justify-center items-center rounded-lg w-full text-sm h-full ${
-		kind === "primary" ? "bg-primary-default text-white" : "bg-white text-primary-default border border-primary-default"
-	}`;
+export default function CustomButton({ text, type = "submit", kind = "primary", href, alt = "button", loading = false }: Props) {
+	const classes = `shadow-[0px_18px_30px_0px_rgba(47,28,28,0.1)] p-3 gap-2 flex justify-center items-center rounded-lg w-full text-sm h-full
+	hover:shadow-[0px_18px_30px_0px_rgba(47,28,28,0.2)] hover:scale-105 transition-all duration-300 border ${
+		kind === "primary" ? "bg-primary-default text-white" : "bg-white text-primary-default  border-primary-default"
+	}
+	${loading ? `cursor-not-allowed bg-opacity-50 ${kind === "primary" && "bg-primary-background-dark !text-primary-default border-2"}` : "cursor-pointer"}	
+	`;
 	const identifier = typeof text === "string" ? text : <Image src={text} alt={alt} width={20} height={20} />;
 	return (
 		<>
@@ -23,8 +27,8 @@ export default function CustomButton({ text, type = "submit", kind = "primary", 
 						{identifier}
 					</Link>
 				) : (
-					<button className={classes} type={type}>
-						{identifier}
+					<button className={classes} type={type} disabled={loading}>
+						{loading ? "Loading..." : identifier}
 					</button>
 				)}
 			</div>
