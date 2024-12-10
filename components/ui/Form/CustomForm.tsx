bf232608be0b3c25c99cdb9fa7b0c782999e6e79
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-12-10 22:22:44 +0200

    feat: Enhance CustomForm with loading state and toast notifications for sign-in process

--- components/ui/Form/CustomForm.tsx
+++ components/ui/Form/CustomForm.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import CustomInput from "../Input/CustomInput";
-import Form from "next/form";
 import Link from "next/link";
 import CustomButton from "../Button/CustomButton";
 import CustomDivider from "../Divider/CustomDivider";
 import IdentityProviders from "@/components/Authentication/IdentityProviders";
+import { signIn } from "next-auth/react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export type FormDetails = {
 	title: string;
@@ -31,16 +33,48 @@ type Props = {
 };
 
 export default function CustomForm({ formDetails }: Props) {
+	const router = useRouter();
+
+	const [loading, setLoading] = useState(false);
+
 	const { title, inputs, link, submitText } = formDetails;
 
-	const handleSubmit = async (formData: FormData) => {
-		console.log("Form submitted:", Object.fromEntries(formData));
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.currentTarget);
+
+		// for (const [key, value] of formData.entries()) {
+		// 	console.log(`Key: ${key}, Value: ${value}`);
+		// }
+
+		const dataToSend = {
+			email: formData.get("email") as string,
+			password: formData.get("password") as string,
+		};
+
+		setLoading(true);
+
+		const res = await signIn("credentials", {
+			email: dataToSend.email,
+			password: dataToSend.password,
+			redirect: false,
+		});
+
+		setLoading(false);
+
+		if (res && res.ok) {
+			toast("Sign-in successful", { type: "success" });
+			router.push("/dashboard");
+		} else {
+			toast(res?.error || "Sign-in failed", { type: "error" });
+		}
 	};
 	return (
 		<>
 			<div className='w-4/5 flex flex-col gap-8'>
 				<h4 className='font-bold text-2xl'>{title}</h4>
-				<Form action={handleSubmit}>
+				<form onSubmit={handleSubmit}>
 					<div className='flex flex-col gap-10'>
 						<div className='flex flex-col gap-4'>
 							<div className='flex flex-col gap-8'>
@@ -48,18 +82,22 @@ export default function CustomForm({ formDetails }: Props) {
 									<CustomInput key={input.id} type={input.type} placeholder={input.placeholder} name={input.name} id={input.id} value={input.value} />
 								))}
 							</div>
-							<p className={`text-[14px] ${link.align}`}>
+							<p
+								className={`text-[14px] 
+									
+								${link.align}`}
+							>
 								{link.description && link.description}
-								<span>
+								<span className='hover:underline '>
 									<Link href={link.href} className='text-primary-default'>
 										{link.text}
 									</Link>
 								</span>
 							</p>
 						</div>
-						<CustomButton text={submitText} />
+						<CustomButton text={submitText} loading={loading} />
 					</div>
-				</Form>
+				</form>
 				<div className='flex flex-col gap-8'>
 					<CustomDivider text='Or Continue With' />
 					<IdentityProviders />
