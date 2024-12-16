08877642d5c853ed2403d67ecf2df221d483b34f
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-12-16 21:20:41 +0200

    refactor: Update NextAuth options for improved readability and consistency

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,70 +2,78 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 declare module "next-auth" {
-	interface User {
-		token: string;
-	}
+  interface User {
+    token: string;
+  }
 
-	interface Session {
-		accessToken: unknown;
-	}
+  interface Session {
+    accessToken: unknown;
+  }
 
-	interface JWT {
-		accessToken: string;
-	}
+  interface JWT {
+    accessToken: string;
+  }
 }
 
-export const options: NextAuthOptions = {
-	providers: [
-		CredentialsProvider({
-			name: "Credentials",
-			credentials: {
-				email: { label: "Email", type: "email", placeholder: "Enter you email" },
-				password: { label: "Password", type: "password" },
-			},
-			async authorize(credentials) {
-				if (!credentials) {
-					throw new Error("Credentials are required");
-				}
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter you email",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          throw new Error("Credentials are required");
+        }
 
-				const { email, password } = credentials;
+        const { email, password } = credentials;
 
-				const body = JSON.stringify({ email, password });
+        const body = JSON.stringify({ email, password });
 
-				const res = await fetch("https://exam.elevateegy.com/api/v1/auth/signin", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body,
-				});
+        const res = await fetch(
+          "https://exam.elevateegy.com/api/v1/auth/signin",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body,
+          }
+        );
 
-				const user = await res.json();
+        const user = await res.json();
 
-				if (res.ok && user) {
-					return user;
-				} else {
-					throw new Error(user?.message || "Invalid email or password");
-				}
-			},
-		}),
-	],
-	pages: {
-		signIn: "/",
-	},
-	session: {
-		strategy: "jwt",
-	},
-	callbacks: {
-		async jwt({ token, user }) {
-			return { ...token, ...user };
-		},
-		async session({ session, token }) {
-			return { ...session, ...token };
-		},
-	},
+        if (res.ok && user) {
+          return user;
+        } else {
+          throw new Error(user?.message || "Invalid email or password");
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      return { ...session, ...token };
+    },
+  },
 };
 
-const handler = NextAuth(options);
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handler;
+export const POST = handler;
