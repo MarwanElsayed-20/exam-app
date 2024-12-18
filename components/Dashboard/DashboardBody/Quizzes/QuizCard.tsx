d60a2455c01d9f9c932e430d1b118be14270fe87
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-12-18 22:06:43 +0200

    feat: Enhance QuizCard with hover effects and improved styling

--- components/Dashboard/DashboardBody/Quizzes/QuizCard.tsx
+++ components/Dashboard/DashboardBody/Quizzes/QuizCard.tsx
@@ -11,9 +11,9 @@ type Props = {
 export default function QuizCard({ subject }: Props) {
 	return (
 		<>
-			<div className='relative'>
-				<Image src={subject.icon} alt={subject.name} width={300} height={300} className='w-full h-full object-contain' />
-				<div className='flex flex-col justify-center p-4   absolute top-[199.78px] left-[27.01px] gap-0 rounded-xl bg-[#1935CA66] backdrop-blur-[27.01px]'>
+			<div className='relative group overflow-hidden cursor-pointer rounded-xl'>
+				<Image src={subject.icon} alt={subject.name} width={300} height={300} className='w-full h-full object-contain group-hover:scale-110 transition-all duration-300' />
+				<div className='flex flex-col justify-center p-4 absolute top-[199.78px] left-[27.01px] gap-0 rounded-xl bg-[#1935CA66] backdrop-blur-[27.01px]'>
 					<h3 className='text-white font-bold text-sm'>{subject.name}</h3>
 					<p className='text-white text-sm'>Voluptatem aut ut dignissimos blanditiis</p>
 				</div>
