9d765c0177bba5278d08faa2453d514d21523251
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-12-18 22:07:03 +0200

    feat: Add NoData component for displaying a message when no data is available

---
+++ components/ui/NoData/NoData.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NoData() {
+	return (
+		<>
+			<div className='flex justify-center items-center h-[50vh]'>
+				<div className='text-center'>
+					<h2 className='text-2xl font-bold'>No Data Found</h2>
+				</div>
+			</div>
+		</>
+	);
+}
