026b1890db7a05794d57ab382bb8e9c176cf8d6a
Marwan Elsayed <marwanellsayed@hotmail.com>
2024-12-18 22:06:18 +0200

    feat: Add placeholder image domain to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["exam.elevateegy.com"],
+		domains: ["exam.elevateegy.com", "placehold.jp"],
 	},
 };
 
